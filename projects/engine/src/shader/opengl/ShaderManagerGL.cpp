b57978f6425470cf0164793a6e60e09b1321cccd
David Goeth <goeth@fim.uni-passau.de>
2016-11-18 01:06:55 +0100

    ShaderInitException is now thrown locally in ShaderGL

--- projects/engine/src/shader/opengl/ShaderManagerGL.cpp
+++ projects/engine/src/shader/opengl/ShaderManagerGL.cpp
@@ -30,26 +30,14 @@ void ShaderManagerGL::loadShaders(const string& folder)
 	//TODO : load shaders from folder!
 	auto playground = make_unique<PlaygroundShaderGL>
 		("vertex.gls", "fragment.glsl");
-	if (!playground || playground.get()->loadingFailed())
-	{
-		throw ShaderInitException("ShaderManagerGL::loadShaders: PlaygroundShader couldn't be loaded!");
-	}
 	shaderMap.insert(make_pair("PLAYGROUND", move(playground)));
 	
 	auto lamp = make_unique<LampShaderGL>
 		("vs_light.gls", "fs_light.glsl");
-	if (!lamp || lamp.get()->loadingFailed())
-	{
-		throw ShaderInitException("ShaderManagerGL::loadShaders: LampShader couldn't be loaded!");
-	}
 	shaderMap.insert(make_pair("LAMP", move(lamp)));
 
 
 	auto simpleLight = make_unique<SimpleLightShaderGL>
 		("vs_simpleLightColor.glsl", "fs_simpleLightColor.glsl");
-	if (!simpleLight || simpleLight.get()->loadingFailed())
-	{
-		throw ShaderInitException("ShaderManagerGL::loadShaders: SimpleLightShader couldn't be loaded!");
-	}
 	shaderMap.insert(make_pair("SIMPLE_LIGHT", move(simpleLight)));
 }
