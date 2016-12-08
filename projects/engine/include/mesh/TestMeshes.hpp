7eb73a4f222244348d001dfc1c4f4b2e32554078
David Goeth <goeth@fim.uni-passau.de>
2016-12-08 19:23:14 +0100

    deleted unused stuff

--- projects/engine/include/mesh/TestMeshes.hpp
+++ projects/engine/include/mesh/TestMeshes.hpp
@@ -2,44 +2,6 @@
 #define TEST_MESHES_HPP
 
 namespace TestMeshes {
-
-	static const std::string TRIANGLE_NAME = "TESTMESHES::TRIANGLE";
-	static unsigned int TRIANGLE_VERTEX_SLICE = 3;
-    static const float triangleVertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    };
-
-	static const std::string COLORED_TRIANGLE_NAME = "TESTMESHES::COLORED_TRIANGLE";
-	static unsigned int COLORED_TRIANGLE_VERTEX_SLICE = 6;
-    static const float coloredTriangleVertices[] = {
-        // Positions			// Colors
-        -0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,
-        0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,
-        0.0f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f
-    };
-
-    static const unsigned int triangleIndices[] = {  // Note that we start from 0!
-        0, 1, 2,   // First Triangle
-    };
-
-	static const std::string TRIANGLE1_NAME = "TESTMESHES::TRIANGLE1";
-	static unsigned int TRIANGLE1_VERTEX_SLICE = 3;
-    static const float triangle1Vertices[] = {
-        0.5f,  0.5f, 0.0f,  // Top Right
-        0.5f, -0.5f, 0.0f,  // Bottom Right,
-        -0.5f,  0.5f, 0.0f   // Top Left 
-    };
-
-	static const std::string TRIANGLE2_NAME = "TESTMESHES::TRIANGLE2";
-	static unsigned int TRIANGLE2_VERTEX_SLICE = 3;
-    static const float triangle2Vertices[] = {
-        0.5f, -0.5f, 0.0f,  // Bottom Right
-        -0.5f, -0.5f, 0.0f,  // Bottom Left
-        -0.5f,  0.5f, 0.0f   // Top Left 
-    };
-
 	static const std::string RECTANGLE_NAME = "TESTMESHES::RECTANGLE";
 	static unsigned int RECTANGLE_VERTEX_SLICE = 8;
     static const float rectangleVertices[] = {
