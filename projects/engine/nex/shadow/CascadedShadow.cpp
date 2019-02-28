ee121407a4708be205d5a5eab43f0c68768f1857
David Goeth <goeth@fim.uni-passau.de>
2019-02-28 21:40:12 +0100

    test rebase

--- projects/engine/nex/shadow/CascadedShadow.cpp
+++ projects/engine/nex/shadow/CascadedShadow.cpp
@@ -26,6 +26,7 @@ CascadedShadow::CascadedShadow(unsigned int cascadeWidth, unsigned int cascadeHe
 	mPCF(pcf)
 {
 	resize(cascadeWidth, cascadeHeight);
+	//test
 }
 
 void CascadedShadow::begin(int cascadeIndex)
