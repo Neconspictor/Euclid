ffcd6f0d44cd35b27dc35a30d1c6d1f9a902ff39
David Goeth <goeth@fim.uni-passau.de>
2019-03-16 02:26:48 +0100

    Added comments to derivate Lanzcos resampling algorithm

--- shaders/opengl/post_processing/downsample_fs.glsl
+++ shaders/opengl/post_processing/downsample_fs.glsl
@@ -27,6 +27,13 @@ void main() {
         sinc(x) * sinc(x/a) = (a * sin(pi * x) * sin(pi * x / a)) / (pi^2 * x^2)
         Assuming a Lanczos constant of 2.0, and scaling values to max out at x = +/- 1.5    
         https://stackoverflow.com/questions/14366672/how-can-i-improve-this-webgl-glsl-image-downsampling-shader
+        
+         Steps: 3/8, 6/8, 9/8, 12/8
+        1 + 2*0.7396427920000761
+        + 2*0.2353466775192183
+        + 2*-0.060095064454162
+        + 2*-0.0636843520279026 = 2.7024201060744596
+        result is normalized by 2.7024201060744596 so that sum of luminance remains 1.0
     
     */
     vec2 firstOffset = texelSize;
@@ -44,17 +51,17 @@ void main() {
     vec2 threeStepsRightTextureCoordinate = fs_in.texCoord + thirdOffset;
     vec2 fourStepsRightTextureCoordinate = fs_in.texCoord + fourthOffset;
     
-    fragColor = texture(sourceTexture, centerTextureCoordinate) * 0.38026;
+    fragColor = texture(sourceTexture, centerTextureCoordinate) * 0.38026; // 1.0 / 2.7024201060744596
 
-    fragColor += texture(sourceTexture, oneStepLeftTextureCoordinate) * 0.27667;
-    fragColor += texture(sourceTexture, oneStepRightTextureCoordinate) * 0.27667;
+    fragColor += texture(sourceTexture, oneStepLeftTextureCoordinate) * 0.27667; //sinc(3/8) * sinc((3/8.0)/2.0) / 2.7024201060744596
+    fragColor += texture(sourceTexture, oneStepRightTextureCoordinate) * 0.27667; //sinc(3/8) * sinc((3/8.0)/2.0) / 2.7024201060744596
 
-    fragColor += texture(sourceTexture, twoStepsLeftTextureCoordinate) * 0.08074;
-    fragColor += texture(sourceTexture, twoStepsRightTextureCoordinate) * 0.08074;
+    fragColor += texture(sourceTexture, twoStepsLeftTextureCoordinate) * 0.08074; //sinc(6/8) * sinc((6/8.0)/2.0) / 2.7024201060744596
+    fragColor += texture(sourceTexture, twoStepsRightTextureCoordinate) * 0.08074; //sinc(6/8) * sinc((6/8.0)/2.0) / 2.7024201060744596
 
-    fragColor += texture(sourceTexture, threeStepsLeftTextureCoordinate) * -0.02612;
-    fragColor += texture(sourceTexture, threeStepsRightTextureCoordinate) * -0.02612;
+    fragColor += texture(sourceTexture, threeStepsLeftTextureCoordinate) * -0.02612; //sinc(9/8) * sinc((9/8.0)/2.0) / 2.7024201060744596
+    fragColor += texture(sourceTexture, threeStepsRightTextureCoordinate) * -0.02612; //sinc(9/8) * sinc((9/8.0)/2.0) / 2.7024201060744596
 
-    fragColor += texture(sourceTexture, fourStepsLeftTextureCoordinate) * -0.02143;
-    fragColor += texture(sourceTexture, fourStepsRightTextureCoordinate) * -0.02143; 
+    fragColor += texture(sourceTexture, fourStepsLeftTextureCoordinate) * -0.02143; //sinc(12/8) * sinc((12/8.0)/2.0) / 2.7024201060744596
+    fragColor += texture(sourceTexture, fourStepsRightTextureCoordinate) * -0.02143; //sinc(12/8) * sinc((12/8.0)/2.0) / 2.7024201060744596
 }
