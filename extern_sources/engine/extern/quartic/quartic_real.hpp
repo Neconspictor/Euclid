bb8c6554a28d085e7ab69d36b46245bba2cecd75
David Goeth <goeth@fim.uni-passau.de>
2019-06-15 00:45:56 +0200

    Removed quartic, as it doesn't calculate rightly

--- extern_sources/engine/extern/quartic/quartic_real.hpp
+++
@@ -1,9 +0,0 @@
-/* Solve Polynomials of up to the fourth degree. (over real numbers)
- * Algorithms by Ferrari, Tartaglia, Cardano, et al. (16th century Italy)
- */
-
-#pragma once
-
-namespace nex {
-	int solve_real_poly(int degree, const double* poly, double* results);
-}
