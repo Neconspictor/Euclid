42cd600b3d52e0403874196cb9de642227cccf8a
David Goeth <goeth@fim.uni-passau.de>
2019-06-15 00:51:19 +0200

    Added algebra class for solving quatrics; Torus - Ray intersection works now correctly

--- projects/engine/nex/math/Torus.cpp
+++ projects/engine/nex/math/Torus.cpp
@@ -1,5 +1,7 @@
-#include "Torus.hpp"
-#include "PolynomialSolver.hpp"
+﻿#include "Torus.hpp"
+#include <nex/math/Ray.hpp>
+#include <nex/math/algebra.hpp>
+#include <algorithm>
 
 nex::Torus::Torus(float innerRadius, float outerRadius) : innerRadius(innerRadius), outerRadius(outerRadius)
 {
@@ -7,6 +9,50 @@ nex::Torus::Torus(float innerRadius, float outerRadius) : innerRadius(innerRadiu
 
 nex::Torus::Torus()
 {
+}
+
+nex::Torus::RayIntersection nex::Torus::intersects(const Ray& ray)
+{
+	RayIntersection result;
+
+	/**
+	 * For derivation see: http://cosinekitty.com/raytrace/chapter13_torus.html
+	 * Note: we use y as the up direction and thus y and z are switched in this implementation.
+	 */
+
+	const double A2 = innerRadius * innerRadius;
+	const double B2 = outerRadius * outerRadius;
+	const auto D = ray.getOrigin();
+	const auto E = ray.getDir();
+
+	const double G = 4.0 * A2 * (E.x*E.x + E.z*E.z); // 4A2(E2x+E2y)
+	const double H = 8.0 * A2 * (D.x*E.x + D.z*E.z); // 8A2(DxEx+DyEy)
+	const double I = 4.0 * A2 * (D.x*D.x + D.z*D.z); //4A2(D2x+D2y)
+
+	//Note: J is 1, since direction vectors of our ray implementation are always normalized
+	const double J = 1.0; //dot(E,E); //E2x+E2y+E2z=|E|2
+	const double K = 2.0 * dot(D,E); //2(DxEx+DyEy+DzEz)=2(D⋅E)
+	const double L = dot(D, D) + A2 - B2; //D2x+D2y+D2z+A2−B2=|D|2+(A2−B2)
+
+	//(J^2)*(u^4) + (2JK)*(u^3)+ (2JL+(K^2)−G)*(u^2) + (2KL−H)u + (L2−I) = 0
+	double solutions[4];
+	result.intersectionCount = nex::algebra::SolveQuarticEquation(J*J,
+		2.0 * J*K,
+		2.0 * J*L + K * K - G,
+		2.0 * K*L - H,
+		L*L - I,
+		solutions);
+
+	std::sort(solutions, solutions + result.intersectionCount);
+
+	for (size_t i = 0; i < result.intersectionCount; ++i)
+	{
+		result.multipliers[i] = static_cast<float>(solutions[i]);
+	}
+
+
+
+	return result;
 }
 
 bool nex::Torus::isInHull(const glm::vec3& p) const
--- projects/engine/nex/math/Torus.hpp
+++ projects/engine/nex/math/Torus.hpp
@@ -4,13 +4,22 @@
 
 namespace nex
 {
+	class Ray;
+
 	struct Torus
 	{
+		struct RayIntersection {
+			size_t intersectionCount = 0;
+			float multipliers[4];
+		};
+
 		float innerRadius = 2.0f;
 		float outerRadius = 0.5f;
 
 		Torus(float innerRadius, float outerRadius);
 		Torus();
+
+		RayIntersection intersects(const Ray& ray);
 
 		/**
 		 * Checks if a point is located inside the torus topology.
