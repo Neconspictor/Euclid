fe733fc710c4dc9de85201067c3098854abfc40e
David Goeth <goeth@fim.uni-passau.de>
2019-07-26 16:51:46 +0200

    Update for VS 16

--- createVS2017Build.bat
+++
@@ -1,3 +0,0 @@
-mkDir build
-cd build
-cmake .. -G "Visual Studio 15 2017 Win64" -DUSE_EUCLID_ALL_OPTIMIZATIONS=ON
---
+++ createVS2019Build.bat
@@ -0,0 +1,3 @@
+mkDir build
+cd build
+cmake .. -G "Visual Studio 16 2019 Win64" -DUSE_EUCLID_ALL_OPTIMIZATIONS=ON
