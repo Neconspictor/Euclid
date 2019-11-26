0a7ebaa9bd769520a4d4c2c2a57639b767fd345b
David Goeth <goeth@fim.uni-passau.de>
2019-11-26 18:32:04 +0100

    Post processing: Changed tonemapping algorithm

--- projects/shaders/opengl/post_processing/postProcess_fs.glsl
+++ projects/shaders/opengl/post_processing/postProcess_fs.glsl
@@ -20,6 +20,75 @@ layout(binding = 6) uniform sampler2D motionMap;
 #define MOTION_SCALE 1.0
 
 const vec4 sepiaColor = vec4(1.2, 1.0, 0.8, 1.0);
+const float gamma = 2.2f;
+
+
+vec3 Uncharted2ToneMapping(vec3 color)
+{
+	float A = 0.15;
+	float B = 0.50;
+	float C = 0.10;
+	float D = 0.20;
+	float E = 0.02;
+	float F = 0.30;
+	float W = 11.2;
+	float exposure = 2.;
+	color *= exposure;
+	color = ((color * (A * color + C * B) + D * E) / (color * (A * color + B) + D * F)) - E / F;
+	float white = ((W * (A * W + C * B) + D * E) / (W * (A * W + B) + D * F)) - E / F;
+	color /= white;
+	color = pow(color, vec3(1. / gamma));
+	return color;
+}
+
+vec3 linearToneMapping(vec3 color)
+{
+	float exposure = 1.;
+	color = clamp(exposure * color, 0., 1.);
+	color = pow(color, vec3(1. / gamma));
+	return color;
+}
+
+vec3 simpleReinhardToneMapping(vec3 color)
+{
+	float exposure = 1.5;
+	color *= exposure/(1. + color / exposure);
+	color = pow(color, vec3(1. / gamma));
+	return color;
+}
+
+vec3 lumaBasedReinhardToneMapping(vec3 color)
+{
+	float luma = dot(color, vec3(0.2126, 0.7152, 0.0722));
+	float toneMappedLuma = luma / (1. + luma);
+	color *= toneMappedLuma / luma;
+	color = pow(color, vec3(1. / gamma));
+	return color;
+}
+
+vec3 whitePreservingLumaBasedReinhardToneMapping(vec3 color)
+{
+	float white = 2.;
+	float luma = dot(color, vec3(0.2126, 0.7152, 0.0722));
+	float toneMappedLuma = luma * (1. + luma / (white*white)) / (1. + luma);
+	color *= toneMappedLuma / luma;
+	color = pow(color, vec3(1. / gamma));
+	return color;
+}
+
+vec3 RomBinDaHouseToneMapping(vec3 color)
+{
+    color = exp( -1.0 / ( 2.72*color + 0.15 ) );
+	color = pow(color, vec3(1. / gamma));
+	return color;
+}
+
+vec3 filmicToneMapping(vec3 color)
+{
+	color = max(vec3(0.), color - vec3(0.004));
+	color = (color * (6.2 * color + .5)) / (color * (6.2 * color + 1.7) + 0.06);
+	return color;
+}
 
 
 void main() {
@@ -29,7 +98,7 @@ void main() {
     
     
     // Bloom
-    const float strength = 0.5;
+    const float strength = 1.0;
     vec3 bloomHalfthSample = clamp(texture(bloomHalfth, fs_in.texCoord).rgb, 0.0, 100.0) * strength;
     vec3 bloomQuarterSample = clamp(texture(bloomQuarter, fs_in.texCoord).rgb, 0.0, 100.0) * strength * 0.75;
     vec3 bloomEigthSample = clamp(texture(bloomEigth, fs_in.texCoord).rgb, 0.0, 100.0) * strength * 0.5;
@@ -59,12 +128,20 @@ void main() {
     
     // HDR tonemapping
     const float exposure = 1;
-    color *= exposure;
-    color = color / (color + vec4(1.0));
-	
-    // gamma correct
-    const float gamma = 2.2f;
-    color = pow(color, vec4(1.0/gamma)); 
+    color.rgb *= exposure;
+    //color.rgb = color.rgb / (color.rgb + vec3(1.0));
+    //color.rgb = pow(color.rgb, vec3(1.0/gamma));
+    
+    vec3 beforeToneMapping = color.rgb;
+    
+    
+    //simple tonemapping
+    vec3 firstTone = beforeToneMapping / (beforeToneMapping + vec3(1.0));
+    firstTone = pow(firstTone, vec3(1.0/gamma));
+    vec3 secondTone = whitePreservingLumaBasedReinhardToneMapping(beforeToneMapping);
+    vec3 thirdTone = Uncharted2ToneMapping(beforeToneMapping);
+    color.rgb = mix(firstTone, secondTone, 0.3);
+    color.rgb = mix(color.rgb, thirdTone, 0.3);
     
     
     
