bad8fd7b061f1f017b53fdffc971d0d1f1559454
David Goeth <goeth@fim.uni-passau.de>
2019-12-05 16:34:42 +0100

    Added utility function for texture atlas uv transformation

---
+++ projects/shaders/opengl/util/texture_atlas.glsl
@@ -0,0 +1,26 @@
+#ifndef TEXTURE_ATLAS_HEADER
+#define TEXTURE_ATLAS_HEADER
+
+/**
+ * Transforms a uv coordinate in texture tile space into texture atlas space.
+ *
+ * @param uv        : UV coordinate in range [0, 1] that should be transformed for a tile in the texture atlas uv space.
+ *                    The transformed result will be written into this parameter.
+ * @param tex       : The texture atlas.
+ * @param tileCount : Specifies the number of tiles in x and y axis direction of the texture atlas.
+ * @param tileIndex : The index of the tile for which the uv coordinates should be transformed. 
+ */
+void toAtlasUvSpace(inout vec2 uv, const in sampler2D tex, const in ivec2 tileCount, const in int tileIndex) 
+{
+    vec2 scale = vec2(1.0) / vec2(tileCount);
+    
+    // Get row and column for retrieving the offset
+    int row = tileIndex / tileCount.x;
+    int column = tileIndex - tileCount.x * row;
+    vec2 offset = scale * vec2(row, column);
+    
+    // transform uv coordinate from tile space into atlas space.
+    uv = uv * scale + offset;
+}
+
+#endif //TEXTURE_ATLAS_HEADER
