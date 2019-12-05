28505823e1c947a866aad5c2fbbe5aaba1ee9e87
David Goeth <goeth@fim.uni-passau.de>
2019-12-05 17:44:36 +0100

    Fixed Macro overwrite in glad.h

--- lib/glad/include/glad/glad.h
+++ lib/glad/include/glad/glad.h
@@ -37,7 +37,8 @@
 #define __gl_h_
 
 #if defined(_WIN32) && !defined(APIENTRY) && !defined(__CYGWIN__) && !defined(__SCITECH_SNAP__)
-#define APIENTRY __stdcall
+#include <Windows.h>
+//#define APIENTRY __stdcall
 #endif
 
 #ifndef APIENTRY
