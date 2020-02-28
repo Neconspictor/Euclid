7990bee19bb530572c71a26cbc3876e6cf4b4aad
David Goeth <goeth@fim.uni-passau.de>
2020-02-28 18:24:49 +0100

    blue print: vob hierarchy is mapped to matrix array indices

--- projects/engine/nex/scene/VobBluePrint.cpp
+++ projects/engine/nex/scene/VobBluePrint.cpp
@@ -1,12 +1,18 @@
 #include <nex/scene/VobBluePrint.hpp>
 
-nex::VobBluePrint::VobBluePrint(std::unique_ptr<Vob> bluePrint) : mVobHierarchy(std::move(bluePrint))
+nex::VobBluePrint::VobBluePrint(std::unique_ptr<Vob> bluePrint) : mVob(std::move(bluePrint))
 {
+	if (!mVob) {
+		throw_with_trace(std::invalid_argument("blue-print vob mustn't be null!"));
+	}
+
+	auto size = fillMap(*mVob, 0) + 1;
+	assert(size == mBluePrintChildVobNameSIDToMatrixIndex.size());
 }
 
 std::unique_ptr<nex::Vob> nex::VobBluePrint::createBluePrint() const
 {
-	auto copy = mVobHierarchy->createBluePrintCopy();
+	auto copy = mVob->createBluePrintCopy();
 
 	copy->setBluePrint(this);
 
@@ -23,4 +29,32 @@ void nex::VobBluePrint::addKeyFrameAnimations(std::unordered_map<nex::Sid, std::
 const std::unordered_map<nex::Sid, std::unique_ptr<nex::KeyFrameAnimation>>& nex::VobBluePrint::getKeyFrameAnimations() const
 {
 	return mKeyFrameAnis;
+}
+
+unsigned nex::VobBluePrint::mapToMatrixArrayIndex(const nex::Vob& vob)
+{
+	if (vob.getBluePrint() != this) {
+		throw_with_trace(std::invalid_argument("Vob isn't connected to the blue-print!" + vob.getName()));
+	}
+
+	const auto sid = vob.getBluePrintNodeNameSID();
+	return mBluePrintChildVobNameSIDToMatrixIndex[sid];
+}
+
+int nex::VobBluePrint::fillMap(const nex::Vob& vob, int currentIndex)
+{
+	const auto sid = SID(vob.getName());
+
+	if (mBluePrintChildVobNameSIDToMatrixIndex.find(sid) != end(mBluePrintChildVobNameSIDToMatrixIndex)) {
+		bool t = false;
+	}
+
+	mBluePrintChildVobNameSIDToMatrixIndex[sid] = currentIndex;
+
+	for (const auto& child : vob.getChildren()) {
+		currentIndex = fillMap(*child.get(), currentIndex + 1);
+	}
+
+	// return the current index
+	return currentIndex;
 }
--- projects/engine/nex/scene/VobBluePrint.hpp
+++ projects/engine/nex/scene/VobBluePrint.hpp
@@ -14,16 +14,46 @@ namespace nex
 	class VobBluePrint {
 	public:
 
+		/**
+		 * Creates a new blue-print.
+		 * @throws std::invalid_argument : if the unique_ptr of the bluePrint vob is empty. 
+		 */
 		VobBluePrint(std::unique_ptr<Vob> bluePrint);
 
+		/**
+		 * Creates a vob hierarchy using the blue-print. The resulting vob will be connected to this blue-print.
+		 */
 		std::unique_ptr<Vob> createBluePrint() const;
 
-
+		/**
+		 * Adds a map of (SID of keyframe animation name, keyframe animation) pairs to this blue-print.
+		 * @throws std::invalid_argument : If one of the keyframe animations is not applicable to vobs of this blue-print.
+		 */
 		void addKeyFrameAnimations(std::unordered_map<nex::Sid, std::unique_ptr<KeyFrameAnimation>> anis);
+		
+		/**
+		 * Provides all registered keyframe animations. The Sid key is the SID of the keyframe animations name.
+		 */
 		const std::unordered_map<nex::Sid, std::unique_ptr<KeyFrameAnimation>>& getKeyFrameAnimations() const;
 
+		/**
+		 * Returns the matrix array index for a keyframe animation matrix array for a vob which is connected to this blue-print.
+		 * @throws std::invalid_argument : If the vob isn't connected to this blue-print.
+		 */
+		unsigned mapToMatrixArrayIndex(const nex::Vob& vob);
+
 	private:
-		std::unique_ptr<Vob> mVobHierarchy;
+		std::unique_ptr<Vob> mVob;
 		std::unordered_map<nex::Sid, std::unique_ptr<KeyFrameAnimation>> mKeyFrameAnis;
+
+		// Maps array index of a matrix array to vob name SIDs of the blue print vob.
+		// This mapping is needed for keyframe animations, which need such a mapping since they operate on a channel to matrix index mapping.
+		// Vobs using this blue print can use this mapping for mapping their own vob hierarchy to the matrix array calculated by the keyframe animation object.
+		// Note: Each child vob created by the blue print has a name SID to a (child) vob of the blue-print vob. This way it is guaranteed to have a consistent 
+		// mapping even if the vob's name is changed afterwards.
+		//std::unordered_map<unsigned, nex::Sid> mMatrixIndexToBluePrintChildVobNameSID;
+		std::unordered_map<nex::Sid, unsigned> mBluePrintChildVobNameSIDToMatrixIndex;
+
+		int fillMap(const nex::Vob& vob, int currentIndex);
 	};
 }
